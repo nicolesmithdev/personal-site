c114a1cff604ac9c094ade4d92ed6d87092ce0c4
nicolesmithweb <nicolesmithweb@me.com>
2022-05-28 07:51:01 -0400

    Reduce width of logo on mobile

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -2,7 +2,12 @@
     <header>
         <div class="container">
             <router-link to="/" id="logo">
-                <img src="../../assets/images/nicole-smith-web-dev.png" alt="Nicole Smith Web Developer" width="488" height="125"/>
+                <img
+                    src="../../assets/images/nicole-smith-web-dev.png"
+                    alt="Nicole Smith Web Developer"
+                    width="488"
+                    height="125"
+                />
             </router-link>
 
             <Arrows v-if="isHome" />
@@ -15,26 +20,24 @@ import Arrows from '../UI/Arrows.vue';
 
 export default {
     components: {
-        Arrows
+        Arrows,
     },
     computed: {
         isHome() {
             return this.$route.name === 'home';
-        }
-    }
-}
+        },
+    },
+};
 </script>
 
 <style lang="scss" scoped>
 header {
-    background: url("../../assets/images/header.jpg") no-repeat center center;
+    background: url('../../assets/images/header.jpg') no-repeat center center;
     background-size: cover;
     border-bottom: 1px solid rgba(0, 0, 0, 0.05);
-
     @include respond-to('medium') {
         background-attachment: fixed;
     }
-
     .home & {
         align-items: center;
         border-bottom: none;
@@ -44,29 +47,28 @@ header {
         justify-content: center;
         position: relative;
         text-align: center;
-
         @include respond-to('x-small') {
-            height: 33vh;
+            height: 25vh;
         }
     }
 }
-
 #logo {
     display: block;
     margin: 0 auto;
     max-width: 100%;
     padding: 2rem 0;
     width: 293px;
-
     .home & {
+        max-width: 50%;
         padding: 0;
         width: 488px;
     }
-}
-
-#logo img {
-    display: block;
-    filter: contrast(0) sepia(100%) hue-rotate(116deg) brightness(2) saturate(0);
-    -webkit-filter: contrast(0) sepia(100%) hue-rotate(116deg) brightness(2) saturate(0);
+    img {
+        display: block;
+        filter: contrast(0) sepia(100%) hue-rotate(116deg) brightness(2)
+            saturate(0);
+        -webkit-filter: contrast(0) sepia(100%) hue-rotate(116deg) brightness(2)
+            saturate(0);
+    }
 }
 </style>

4de9f7cb98f51a05ae2a095656843e66f1e87d2e
nicolesmithweb <nicolesmithweb@me.com>
2022-05-28 07:36:50 -0400

    Add skills logos

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,30 +1,31 @@
 <template>
     <div>
-      <panel id="about">
-        <AboutPanel />
-      </panel>
-      <panel id="quote">
-        <QuotePanel />
-      </panel>
-      <panel id="uses">
-        <UsesPanel />
-      </panel>
+        <panel v-for="panel in panels" :key="panel" :id="panel.toLowerCase()">
+            <component :is="panel + 'Panel'" />
+        </panel>
     </div>
 </template>
 
 <script>
-import AboutPanel from '../panels/PanelAbout.vue';
-import QuotePanel from '../panels/PanelQuote.vue';
-import UsesPanel from '../panels/PanelUses.vue';
+import AboutPanel from '../panels/PanelAbout';
+import SkillsPanel from '../panels/PanelSkills';
+import QuotePanel from '../panels/PanelQuote';
+import UsesPanel from '../panels/PanelUses';
 
 export default {
-  metaInfo: {
-    titleTemplate: null
-  },
-  components: {
-    AboutPanel,
-    QuotePanel,
-    UsesPanel
-  }
-}
+    metaInfo: {
+        titleTemplate: null,
+    },
+    components: {
+        AboutPanel,
+        SkillsPanel,
+        QuotePanel,
+        UsesPanel,
+    },
+    data() {
+        return {
+            panels: ['About', 'Skills', 'Quote', 'Uses'],
+        };
+    },
+};
 </script>
---
+++ src/components/panels/PanelSkills.vue
@@ -0,0 +1,51 @@
+<template>
+    <p>
+        <img
+            v-for="skill in skills"
+            :key="skill"
+            :src="require(`../../assets/images/${skill}.svg`)"
+            :alt="skill"
+            :title="skill"
+            width="90"
+            height="90"
+        />
+    </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            skills: [
+                'html',
+                'css',
+                'javascript',
+                'sass',
+                'vue',
+                'react',
+                'nodejs',
+                'php',
+                'mysql',
+                'wordpress',
+            ],
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+#skills {
+    background-color: $light-gray;
+    p {
+        align-items: center;
+        display: flex;
+        flex-wrap: wrap;
+        grid-gap: 2rem;
+        justify-content: center;
+        img {
+            max-height: 90px;
+            max-width: 90px;
+        }
+    }
+}
+</style>
